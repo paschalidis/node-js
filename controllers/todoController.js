f55e67951e472d79acfa27c95448f027deffa1a3
Pavlos <msf7415012@teilar.gr>
2017-11-05 15:08:47 +0200

    Add mongoose and connect to mongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,40 @@
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// Content to database
+mongoose.connect('');
+
+// Create a schema - blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+// Create model
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if(err) throw err;
+    console.log('Item saved');
+});
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding add'}];
+
 module.exports = function(app){
     
     app.get('/todo', function(req, res){
-        console.log(req.url);
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo', function(req, res){
-        
+    app.post('/todo', urlencodedParser, function(req, res){
+        data.push(req.body);
+        res.json(data);
     });
 
-    app.delete('/todo', function(req, res){
-
+    app.delete('/todo/:item', function(req, res){
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
     });
 };
