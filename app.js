6ee9c292657119f586871717a130b03ff1fbd08e
Pavlos <msf7415012@teilar.gr>
2017-11-05 13:55:55 +0200

    Todo page

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var express = require('express');
 var todoController = require('./controllers/todoController');
 var app = express();
 
-// Set up teplate egine
-app.set('view engine', 'ets');
+// Set up template engine
+app.set('view engine', 'ejs');
 
 // Static files
 app.use(express.static('./public'));
 
 // Fire Controllers
 todoController(app);
+
+app.listen(3000);
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,8 @@
 module.exports = function(app){
     
     app.get('/todo', function(req, res){
-
+        console.log(req.url);
+        res.render('todo');
     });
 
     app.post('/todo', function(req, res){
